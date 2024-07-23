99b337c2ffd6d29da3dc481c955d450f1e51f4f0
Sudhir Rai <sudhirrai128@gmail.com>
2024-07-24 01:15:39 +0530

    Server setup

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
@@ -7,7 +6,6 @@ import connectToDB from './db/db.js'
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT
-const __dirname = path.resolve()
 
 
 app.use(express.json())
@@ -21,14 +19,6 @@ app.use("/api/auth",authRoute )
 app.use("/api/task", taskRoute)
 app.use("/api/contact", contactRoute)
 
-
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "/Client/dist")))
-
-    app.get("*", (req, res)=>{
-        res.sendFile(path.resolve(__dirname, "Client", "dist", "index.html"))
-    })
-}
 
 app.get("/", (req, res)=>{
     res.json({hello: "Working nice"})
